243d480d8d123330742ef09d416045fac91745ce
Kelbin <kiibynlhune77@gmail.com>
2021-07-02 11:47:29 +0700

    heroku deploy

--- API/Extension/ApplicationServicesExtension.cs
+++ API/Extension/ApplicationServicesExtension.cs
@@ -46,7 +46,7 @@ namespace API.Extension
                 {
                     // Use connection string provided at runtime by Heroku.
                     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
+                    
                     // Parse connection URL to connection string for Npgsql
                     connUrl = connUrl.Replace("postgres://", string.Empty);
                     var pgUserPass = connUrl.Split("@")[0];
